7d277ca57859ec5d1c5c21cc9bc3e05575e64d43
sebastian <sebastian@kallisto.fritz.box>
2019-04-20 11:40:52 +0200

    init common context

---
+++ slag-common/slag-common-context/src/main/java/de/slag/common/context/SlagContextException.java
@@ -0,0 +1,11 @@
+package de.slag.common.context;
+
+public class SlagContextException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public SlagContextException(String s) {
+		super(s);
+	}
+
+}
