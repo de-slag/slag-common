f2d0a4d26dfbe58808cee6bf0f99cd73c22e3ed3
sebastian <sebastian@kallisto.fritz.box>
2019-04-21 12:48:30 +0200

    +DateUtils: easter sunday &test

---
+++ slag-common/slag-common-utils/src/test/java/DateUtilsTest.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.slag.common.utils.DateUtils;
+
+public class DateUtilsTest {
+	
+	@Test
+	public void test() {
+		final LocalDate easterSunday = DateUtils.getEasterSunday(Year.of(2019));
+		Assert.assertTrue(easterSunday.getMonth() == Month.APRIL);
+		Assert.assertTrue(easterSunday.getDayOfMonth() == 21);
+	}
+
+}
