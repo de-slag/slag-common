8643ea9d398bdb8b1dd507b717bcffe5eb739799
sebastian <sebastian@enceladus.fritz.box>
2019-05-20 12:16:36 +0200

    missing dependencies

--- slag-common/slag-common-reflect/src/main/java/de/slag/common/reflect/FieldMappingUtils.java
+++ slag-common/slag-common-reflect/src/main/java/de/slag/common/reflect/FieldMappingUtils.java
@@ -14,6 +14,7 @@ import org.apache.commons.logging.LogFactory;
 import de.slag.common.base.BaseException;
 import de.slag.common.base.MessageCacheable;
 
+
 public class FieldMappingUtils {
 
 	private static final Log LOG = LogFactory.getLog(FieldMappingUtils.class);
