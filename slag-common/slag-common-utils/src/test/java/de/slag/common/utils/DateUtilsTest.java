789f9b6c7672e73fc324c98cf08d437382ed0fca
sebastian <sebastian@enceladus.fritz.box>
2019-06-03 04:50:19 +0200

    renew DateUtilsTest

---
+++ slag-common/slag-common-utils/src/test/java/de/slag/common/utils/DateUtilsTest.java
@@ -0,0 +1,70 @@
+package de.slag.common.utils;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Year;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+
+class DateUtilsTest {
+
+
+	@Ignore
+	@Test
+	void testToCalendarLocalDate() {
+		final Calendar instance = Calendar.getInstance();
+		instance.set(2019, 3, 1, 0, 0, 0);
+		final Calendar calendar = DateUtils.toCalendar(LocalDate.of(2019, 4, 1));
+		Assert.assertThat(calendar, Matchers.is(instance));
+		
+	}
+
+	@Ignore
+	@Test
+	void testToCalendarDate() {
+		final Calendar instance = Calendar.getInstance();
+		instance.setTimeInMillis(0);
+		final Calendar calendar = DateUtils.toCalendar(new Date(0));
+		
+	}
+
+	@Ignore
+	@Test
+	void testToDateLocalDateTime() {
+		final Date date = DateUtils.toDate(LocalDateTime.of(1970, 1, 1, 0, 0, 0));
+	}
+
+	@Ignore
+	@Test
+	void testToDateLocalDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToLocalDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToLocalDateTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEasterSundayInt() {
+		Assert.assertThat(DateUtils.getEasterSunday(2019), Matchers.is(LocalDate.of(2019, 4, 21)));
+	}
+
+	@Test
+	void testGetEasterSundayYear() {
+		Assert.assertThat(DateUtils.getEasterSunday(Year.of(2019)), Matchers.is(LocalDate.of(2019, 4, 21)));
+	}
+
+}
