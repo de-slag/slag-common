466229230a00819a3bb003a59b4baad84181ab76
slipp <slipp@NB308.coinor.local>
2019-06-28 10:55:02 +0200

    + method to find all

--- slag-common/slag-common-model/src/main/java/de/slag/common/Dao.java
+++ slag-common/slag-common-model/src/main/java/de/slag/common/Dao.java
@@ -3,13 +3,18 @@ package de.slag.common;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public interface Dao<T> {
-	
+
 	void save(T t);
-	
+
 	Optional<T> loadById(Long id);
 
 	Collection<Long> findAllIds();
+
+	default Collection<T> findAll() {
+		return findAllIds().stream().map(id -> loadById(id).get()).collect(Collectors.toList());
+	}
 
 }
