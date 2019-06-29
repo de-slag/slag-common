76759bf79c5d7cd2125b281c839225980258098c
sebastian <sebastian@phobos.speedport.ip>
2019-06-29 14:07:08 +0200

    + SystemLog.toString

--- slag-common/slag-common-model/src/main/java/de/slag/common/model/beans/SystemLog.java
+++ slag-common/slag-common-model/src/main/java/de/slag/common/model/beans/SystemLog.java
@@ -1,10 +1,35 @@
 package de.slag.common.model.beans;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import de.slag.common.model.EntityBean;
 
 @Entity
 public class SystemLog extends EntityBean {
+
+	@Column
+	private String info;
+
+	public SystemLog() {
+		super();
+	}
+
+	public SystemLog(String info) {
+		this.info = info;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	@Override
+	public String toString() {
+		return "SystemLog [getCreatedAt()=" + getCreatedAt() + ", info=" + info + "]";
+	}
 
 }
