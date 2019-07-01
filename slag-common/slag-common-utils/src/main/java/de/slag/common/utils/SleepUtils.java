c7dc133c1b713dc065b55b0ba1fe428aeae61297
sebastian <sebastian@phobos.speedport.ip>
2019-07-01 20:58:49 +0200

    refactoring logging info -> debug

--- slag-common/slag-common-utils/src/main/java/de/slag/common/utils/SleepUtils.java
+++ slag-common/slag-common-utils/src/main/java/de/slag/common/utils/SleepUtils.java
@@ -9,7 +9,7 @@ public class SleepUtils {
 	private static final Log LOG = LogFactory.getLog(SleepUtils.class);
 
 	public static void sleepFor(int sleeptimeInMs) {
-		LOG.info("sleep for (ms):" + sleeptimeInMs);
+		LOG.debug("sleep for (ms):" + sleeptimeInMs);
 		try {
 			Thread.sleep(sleeptimeInMs);
 		} catch (final InterruptedException e) {
