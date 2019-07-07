288db20356f093ed8c4b2d8f691b57566c77469c
slipp <slipp@NB308.coinor.local>
2019-07-07 10:38:23 +0200

    SysLogService +default Methods

--- slag-common/slag-common-model/src/main/java/de/slag/common/model/service/SysLogService.java
+++ slag-common/slag-common-model/src/main/java/de/slag/common/model/service/SysLogService.java
@@ -2,14 +2,26 @@ package de.slag.common.model.service;
 
 import java.util.Collection;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+import de.slag.common.Dao;
+import de.slag.common.model.beans.SysLog;
 import de.slag.common.model.beans.SysLog.Severity;
 import de.slag.common.model.beans.SysLogEntry;
 
 public interface SysLogService {
 
-	void log(Severity severity, String info);
+	default void log(Severity severity, String info) {
+		final SysLog sysLog = new SysLog();
+		sysLog.setSeverity(severity);
+		sysLog.setInfo(info);
+		getDao().save(sysLog);
 
-	Collection<SysLogEntry> findBy(Predicate<SysLogEntry> filter);
+	}
 
+	default Collection<SysLogEntry> findBy(Predicate<SysLogEntry> filter) {
+		return getDao().findAll().stream().filter(filter).collect(Collectors.toList());
+	}
+
+	Dao<SysLog> getDao();
 }
