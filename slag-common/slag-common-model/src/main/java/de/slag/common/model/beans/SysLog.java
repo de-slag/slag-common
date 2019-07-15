11a550f19716acfcbeb2089859ee366aae63dbdb
slipp <slipp@NB308.coinor.local>
2019-07-15 05:18:17 +0200

    SysLog: info column length 4000

--- slag-common/slag-common-model/src/main/java/de/slag/common/model/beans/SysLog.java
+++ slag-common/slag-common-model/src/main/java/de/slag/common/model/beans/SysLog.java
@@ -10,44 +10,38 @@ import de.slag.common.model.EntityBean;
 @Entity
 public class SysLog extends EntityBean implements SysLogEntry {
 
-	@Column
+	@Column(length = 4000)
 	private String info;
 
 	@Enumerated(EnumType.STRING)
 	private Severity severity;
-	
-	
-
-
-
 
 	public String getInfo() {
 		return info;
 	}
 
-
-
 	public void setInfo(String info) {
 		this.info = info;
 	}
-
-
 
 	public Severity getSeverity() {
 		return severity;
 	}
 
-
-
 	public void setSeverity(Severity severity) {
 		this.severity = severity;
 	}
-	
+
 	public enum Severity {
 		INFO,
-		
+
 		WARN,
-		
+
 		ERROR
+	}
+
+	@Override
+	public String toString() {
+		return "SysLog ["+getCreatedAt() + ", " + severity + ": " + info + "]";
 	}
 }
