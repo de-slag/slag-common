e1fd810c4c006a848d0e9c91c43d250f7f633a5b
sebastian <sebastian@phobos.speedport.ip>
2019-07-19 16:27:49 +0200

    +method getValue without Type

--- slag-common/slag-common-api/src/main/java/de/slag/common/api/ReflectionService.java
+++ slag-common/slag-common-api/src/main/java/de/slag/common/api/ReflectionService.java
@@ -6,8 +6,8 @@ public interface ReflectionService {
 	
 	<T> Optional<T> getValue(Object o, String attribute, Class<T> returnType);
 	
+	Object getValue(Object o, String attributeName);
+
 	void setValue(Object o, String attribute, Object value);
-	
-	
 
 }
