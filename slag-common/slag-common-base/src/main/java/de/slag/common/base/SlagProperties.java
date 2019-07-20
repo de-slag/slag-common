fbe691e5c08777298977d25c35a7b0e2a270ee51
sebastian <sebastian@phobos.speedport.ip>
2019-07-20 06:47:07 +0200

    configurable cfg file (via vm args)

--- slag-common/slag-common-base/src/main/java/de/slag/common/base/SlagProperties.java
+++ slag-common/slag-common-base/src/main/java/de/slag/common/base/SlagProperties.java
@@ -5,33 +5,39 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
 
 public final class SlagProperties {
 
+	private static final String SLAG_CFG = "SlagCfg";
+
 	public static Properties getConfigProperties() {
-		final File userHome = SystemUtils.getUserHome();
-		String absolutePath = userHome.getAbsolutePath();
-		String file = "slag.cfg";
-		String cfgFile = absolutePath + "/" + file;
-	
-		if (!new File(cfgFile).exists()) {
-			try {
-				new File(cfgFile).createNewFile();
-			} catch (IOException e) {
-				throw new BaseException(e);
-			}
+		final String slagCfg = System.getProperty(SLAG_CFG);
+		final String cfgFileName;
+
+		if (StringUtils.isBlank(slagCfg)) {
+			final File userHome = SystemUtils.getUserHome();
+			String absolutePath = userHome.getAbsolutePath();
+			String file = "slag.cfg";
+			cfgFileName = absolutePath + "/" + file;
+		} else {
+			cfgFileName = slagCfg;
 		}
-	
+
+		if (!new File(cfgFileName).exists()) {
+			throw new BaseException(String.format("cfg-file not found: '%s'", cfgFileName));
+		}
+
 		final Properties properties = new Properties();
-		try (FileInputStream fis = new FileInputStream(cfgFile)) {
+		try (FileInputStream fis = new FileInputStream(cfgFileName)) {
 			properties.load(fis);
 		} catch (IOException e) {
 			throw new BaseException(e);
 		}
 		return properties;
 	}
-	
+
 	public static String get(String propertyKey) {
 		return getConfigProperties().getProperty(propertyKey);
 	}
