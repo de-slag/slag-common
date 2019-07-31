c2661a26fdd0dc62ab44ea4f69b3c04e46797a78
slipp <slipp@NB308.coinor.local>
2019-07-31 18:41:44 +0200

    AbstractAdmServiceImpl

--- slag-common/slag-common-base/src/main/java/de/slag/common/base/adm/AbstractAdmServiceImpl.java
+++ slag-common/slag-common-base/src/main/java/de/slag/common/base/adm/AbstractAdmServiceImpl.java
@@ -1,5 +1,28 @@
 package de.slag.common.base.adm;
 
-public class AbstractAdmServiceImpl {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public abstract class AbstractAdmServiceImpl implements AdmService {
+
+	private Map<String, String> map = new HashMap<>();
+
+	public AbstractAdmServiceImpl() {
+		this(() -> new HashMap<>());
+	}
+
+	public AbstractAdmServiceImpl(Supplier<Map<String, String>> admSuplier) {
+		this.map.putAll(admSuplier.get());
+	}
+
+	@Override
+	public Optional<String> get(String key) {
+		if (!map.containsKey(key)) {
+			return Optional.empty();
+		}
+		return Optional.of(map.get(key));
+	}
 
 }
