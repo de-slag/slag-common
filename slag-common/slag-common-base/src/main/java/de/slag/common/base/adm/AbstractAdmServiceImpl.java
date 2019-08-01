c1c4091300641523353a484490a0d6482d895c5c
sebastian <sebastian@phobos.speedport.ip>
2019-08-01 06:35:24 +0200

    adm service

--- slag-common/slag-common-base/src/main/java/de/slag/common/base/adm/AbstractAdmServiceImpl.java
+++ slag-common/slag-common-base/src/main/java/de/slag/common/base/adm/AbstractAdmServiceImpl.java
@@ -3,22 +3,36 @@ package de.slag.common.base.adm;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public abstract class AbstractAdmServiceImpl implements AdmService {
 
 	private Map<String, String> map = new HashMap<>();
 
+	private Function<String, String> admFunction;
+
 	public AbstractAdmServiceImpl() {
 		this(() -> new HashMap<>());
 	}
 
 	public AbstractAdmServiceImpl(Supplier<Map<String, String>> admSuplier) {
-		this.map.putAll(admSuplier.get());
+		this(key -> admSuplier.get().get(key));
+	}
+
+	public AbstractAdmServiceImpl(Function<String, String> admFunction) {
+		this.admFunction = admFunction;
 	}
 
 	@Override
 	public Optional<String> get(String key) {
+		if (!map.containsKey(key)) {
+			final String value = admFunction.apply(key);
+			if(value != null) {
+				map.put(key, value);
+			}			
+		}
+		
 		if (!map.containsKey(key)) {
 			return Optional.empty();
 		}
