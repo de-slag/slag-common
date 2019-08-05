04315b4c6383ec2c343f33a09c21d91ac00ee104
sebastian <sebastian@phobos.speedport.ip>
2019-08-05 22:15:38 +0200

    AbstractMockitoRunnerTest

---
+++ slag-common/slag-common-test/src/main/java/de/slag/test/AbstractMockitoRunnerTest.java
@@ -0,0 +1,9 @@
+package de.slag.test;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AbstractMockitoRunnerTest {
+
+}
