bd2968d5345c0bc48fe011c1545461bc66f9e5ec
post@sebastianlipp.de <post@sebastianlipp.de>
2020-10-30 08:39:22 +0100

    support an entity supplier without context dependency

--- slag-common/slag-common-core/src/main/java/de/slag/common/core/dao/AbstractDao.java
+++ slag-common/slag-common-core/src/main/java/de/slag/common/core/dao/AbstractDao.java
@@ -1,27 +1,34 @@
 package de.slag.common.core.dao;
 
+import java.util.Collection;
+import java.util.function.Supplier;
+
 import de.slag.common.core.datasource.DataSource;
 import de.slag.common.core.datasource.DataSourceBuilder;
 import de.slag.common.model.EntityBean;
 import de.slag.common.model.EntityBeanUtils;
 
 public abstract class AbstractDao<E extends EntityBean> implements Dao<E> {
-	
+
 	private static DataSource<EntityBean> dataSource;
-	
+
+	protected static Supplier<Collection<Class<? extends EntityBean>>> registeredEntitiesSupplier;
+
 	static {
-		dataSource = new DataSourceBuilder().build();
+		dataSource = new DataSourceBuilder()
+				.withRegisteredClasses(registeredEntitiesSupplier.get())
+				.build();
 	}
-	
+
 	@Override
 	public E save(E e) {
-		if(e.getId() == null) {
-			dataSource.create(e);			
+		if (e.getId() == null) {
+			dataSource.create(e);
 		}
 		dataSource.update(e);
 		return e;
 	}
-	
+
 	@Override
 	public void delete(E e) {
 		EntityBeanUtils.setDelete(e);
