7ee69652baada4fa9207d9dc791b49b890874c98
post@sebastianlipp.de <post@sebastianlipp.de>
2020-10-30 11:58:12 +0100

    save logic in datasource

--- slag-common/slag-common-core/src/main/java/de/slag/common/core/datasource/DbDataSource.java
+++ slag-common/slag-common-core/src/main/java/de/slag/common/core/datasource/DbDataSource.java
@@ -1,10 +1,12 @@
 package de.slag.common.core.datasource;
 
+import java.util.Optional;
+
 import de.slag.common.db.HibernateSupport;
 import de.slag.common.model.EntityBean;
 
 class DbDataSource implements DataSource<EntityBean> {
-	
+
 	private HibernateSupport hibernateSupport;
 
 	public DbDataSource(HibernateSupport hibernateSupport) {
@@ -15,27 +17,28 @@ class DbDataSource implements DataSource<EntityBean> {
 	@Override
 	public void create(EntityBean t) {
 		hibernateSupport.save(t);
-		
+
 	}
 
 	@Override
 	public EntityBean read(Class<? extends EntityBean> type, Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<? extends EntityBean> loadById = hibernateSupport.loadById(id, type);
+		if (loadById.isEmpty()) {
+			return null;
+		}
+		return loadById.get();
 	}
 
 	@Override
 	public EntityBean update(EntityBean t) {
-		// TODO Auto-generated method stub
-		return null;
+		hibernateSupport.save(t);
+		return t;
 	}
 
 	@Override
 	public void delete(EntityBean t) {
-		// TODO Auto-generated method stub
-		
-	}
+		throw new UnsupportedOperationException("delete");
 
-	
+	}
 
 }
