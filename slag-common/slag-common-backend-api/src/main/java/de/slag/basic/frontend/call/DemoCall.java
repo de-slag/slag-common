d4fa827fb1dd3cb3ee34d3080622ba4bbc493c90
post@sebastianlipp.de <post@sebastianlipp.de>
2020-11-13 07:29:23 +0100

    added democall +builder

---
+++ slag-common/slag-common-backend-api/src/main/java/de/slag/basic/frontend/call/DemoCall.java
@@ -0,0 +1,7 @@
+package de.slag.basic.frontend.call;
+
+import java.util.concurrent.Callable;
+
+public interface DemoCall extends Callable<String>{
+
+}
---
+++ slag-common/slag-common-backend-api/src/main/java/de/slag/basic/frontend/call/builder/DemoCallBuilder.java
@@ -0,0 +1,54 @@
+package de.slag.basic.frontend.call.builder;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import de.slag.basic.frontend.BasicWebTargetCall;
+import de.slag.basic.frontend.BasicWebTargetCallBuilder;
+import de.slag.basic.frontend.BasicWebTargetCallBuilder.HttpMethod;
+import de.slag.basic.frontend.Builder;
+import de.slag.basic.frontend.call.DemoCall;
+
+public class DemoCallBuilder implements Builder<DemoCall> {
+
+	private String target;
+
+	private String endpoint = "demo/ok";
+
+	private HttpMethod httpMethod = HttpMethod.GET;
+
+	private String acceptedResponseType = MediaType.TEXT_PLAIN;
+
+	/**
+	 * 
+	 * @param target - a target url like 'http://host:port/path/'
+	 * @return {@link DemoCallBuilder}
+	 */
+	public DemoCallBuilder withTarget(String target) {
+		this.target = target;
+		return this;
+	}
+
+	@Override
+	public DemoCall build() {
+		if (target == null) {
+			throw new RuntimeException("target not setted");
+		}
+		
+		final BasicWebTargetCall targetCall = new BasicWebTargetCallBuilder()
+				.withEndpoint(endpoint)
+				.withTarget(target)
+				.withMethod(httpMethod)
+				.withAcceptedResponseType(acceptedResponseType)
+				.build();
+		return new DemoCall() {
+
+			@Override
+			public String call() throws Exception {
+				final Response call = targetCall.call();
+				return call.readEntity(String.class);
+			}
+		};
+	}
+
+}
