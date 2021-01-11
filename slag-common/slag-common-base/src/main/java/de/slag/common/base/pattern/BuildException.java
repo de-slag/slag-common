4e96427daf2bb3c93491f1af4e86440158878783
post@sebastianlipp.de <post@sebastianlipp.de>
2021-01-11 07:06:35 +0100

    added build exception

---
+++ slag-common/slag-common-base/src/main/java/de/slag/common/base/pattern/BuildException.java
@@ -0,0 +1,11 @@
+package de.slag.common.base.pattern;
+
+public class BuildException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public BuildException(String s) {
+		super(s);
+	}
+
+}
